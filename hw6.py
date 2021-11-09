a6bb65c7f518f526246462254817ad2ebbabea18
yuriy5139 <yuriy5139@gmail.com>
2021-11-09 21:08:46 +0300

    hw6 in progress

--- hw6.py
+++ hw6.py
@@ -1,3 +1,6 @@
+import sys
+import platform
+
 """
 Рассматриваемая задача на предмет подсчета памяти:
 
@@ -5,10 +8,8 @@
 Примечание: 8 разных ответов.
 """
 
-import sys
-import platform
-
-print("\nО системе:")
+print("-" * 80)
+print("О системе:")
 print(sys.version)
 print(platform.platform())
 print("-" * 80)
@@ -20,14 +21,30 @@ Windows-10-10.0.19041-SP0
 """
 
 
-# Функция - счетчик
-def memcalc(vars):
-    print("\nСводка об использовании памяти:")
+# Счетчик памяти
+def memcalc(vrs, level=0, debug=False):
     total = 0
-    for k, v in vars.items():
-        print(f"\t--переменная {k} имеет тип {type(v).__name__} и использует {sys.getsizeof(v)} байт")
-        total += sys.getsizeof(v)
-    print(f"  Общий объем используемой памяти равен {total} байт\n")
+    indent = " " * (4 * level) + "->"
+    if debug: print(f"{indent}Уровень{level} На входе переменная типа {type(vrs)}, ее объем {sys.getsizeof(vrs)}")
+    total += sys.getsizeof(vrs)
+    if debug: print(f"{indent}Уровень{level} Прибавили ее объем, теперь total равен", total)
+    if isinstance(vrs, list):
+        if debug: print(f"{indent}Уровень{level} Заходим в список")
+        for item in vrs:
+            level += 1
+            total += memcalc(item, level=level, debug=debug)
+            level -= 1
+            if debug: print(f"{indent}Уровень{level} Прибавили ее объем, теперь total равен", total)
+        if debug: print(f"{indent}Уровень{level} Вышли из списка, теперь total равен", total)
+    elif isinstance(vrs, dict):
+        if debug: print(f"{indent}Уровень{level} Заходим в словарь")
+        for val in vrs.values():
+            level += 1
+            total += memcalc(val, level=level, debug=debug)
+            level -= 1
+            if debug: print(f"{indent}Уровень{level} Прибавили ее объем, теперь total равен", total)
+        if debug: print(f"{indent}Уровень{level} Вышли из словаря, теперь total равен", total)
+    return total
 
 
 # Реализация 1 - вложенный цикл и сохранение счетчика в словарь и использованием целочисленного деления
@@ -46,7 +63,8 @@ def implementation1():
     for k, v in multiplicity.items():
         print(f"Числу {k} кратны {v} чисел в диапазоне от 2 до 99")
 
-    return vars()
+    print(f"\nВ реализации используются следующие переменные {vars()}\n")
+    return list(vars().values())
 
 
 # Реализация 2 - вложенный цикл с сохранением результата в словарь и проверкой на превышение границы
@@ -61,12 +79,13 @@ def implementation2():
     for k, v in multiplicity.items():
         print(f"Числу {k} кратны {v} чисел в диапазоне от 2 до 99")
 
-    return vars()
+    print(f"В реализации используются следующие переменные {vars()}")
+    return list(vars().values())
 
 
 # Реализация 3. Создадим словарь со списками в значениях.
-# Каждый список будет содержать конкретные числа, которые делится данное число
-# Чтобы получить ответ, просто выведем длину списка
+# Каждый список будет содержать конкретные числа, которые делятся на данное число
+# Чтобы получить ответ, будем использовать длину списка
 
 def implementation3():
     range2_10 = range(2, 10)
@@ -80,21 +99,21 @@ def implementation3():
     for k, v in numbers.items():
         print(f"Числу {k} кратны {len(v)} чисел в диапазоне от 2 до 99")
 
-    return vars()
+    print(f"В реализации используются следующие переменные {vars()}")
+    return list(vars().values())
 
 
 if __name__ == "__main__":
+
+    # Если выставить в True, в функции memcalc() будет массивный вывод в консоль!
+    DEBUG = False
+
     implementations = [implementation1, implementation2, implementation3]
     for impl in implementations:
-        print("Выполняем реализацию", impl.__name__)
-        memcalc(impl())
+        print("\n *** Проверяем реализацию", impl.__name__)
+        print(f"Общий объем используемой памяти равен {memcalc(impl(), debug=DEBUG)} байт")
 
 """
-Вывод: реализация 1 использует на 48 байт больше памяти, так как в ней вместо цикла while используется
-for, который требует range. Реализация 2 использует while, поэтому она экономит 48 байт и использует 520 байт вместо
-568. 
-Реализация implementation3 менее эффективна с точки зрения памяти, чем первые две, так как сохраняет не только счетчики, 
-но и сами значения чисел, делимых на данное, в словарь. Вычитывая данные из словаря, мы имеем дело со списками, которые
-приходится сохранять в переменную v типа list, занимающую 184 байт. В результате реализация 3 дает суммарное использование памяти
-724 байт. 
+Вывод:
+
 """
