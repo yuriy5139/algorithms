ee05a1243fba5e5d83d21bb0381285f8b4a0b3d9
yuriy5139 <yuriy5139@gmail.com>
2021-11-12 10:09:44 +0300

    dejkstra rewritten without recursion which gave us a bug

--- hw8_task2.py
+++ hw8_task2.py
@@ -3,57 +3,49 @@
 которые необходимо обойти.
 """
 
-# копировать код преподавателя с экрана было неудобно - попробуем написать алгоритм самостоятельно :)
-# будем работать с графом, который использовался на лекции
-
-# В алгоритме нам придется обходить вершины и проверять пути до соседей. Это лучше сделать списками смежности.
-# Т.е., нужно хранить для каждой вершины пару (номер связанной вершины, стоимость маршрута).
-
-from collections import namedtuple, deque
+from collections import namedtuple
+from copy import copy
 
 Vertex = namedtuple('Vertex', ['id', 'cost'])
 g = {0: [Vertex(3, 1), Vertex(2, 1), Vertex(4, 9)],
      1: [Vertex(3, 4), Vertex(2, 9), Vertex(6, 7)],
      2: [Vertex(1, 9), Vertex(4, 3), Vertex(6, 7)],
+     3: [],
      4: [Vertex(6, 1)],
      5: [Vertex(6, 1)],
      6: [Vertex(2, 6), Vertex(4, 8), Vertex(5, 5)],
      7: [Vertex(5, 1), Vertex(6, 2)]}
 
-# Кратчайший маршрут нужно искать из списка возможных, в каждом из которых есть вершина и стоимость до нее.
-# При обходе графа помимо стоимости вершины будем сохранять путь, через который эта стоимость получена.
+Route = namedtuple('Route', ['cost', 'hops'])
 
-Routes = namedtuple('Routes', ['vertex_id', 'hops'])
+
+def process_ngbhrs(g, start,  current, routes, completed):
+    if current in routes:
+        curr_cost = routes[current].cost
+    else:
+        curr_cost = 0
+
+    print(f"entering node {current}, curr_cost = {curr_cost}")
+
+    for ngbh in sorted(g[current], key=lambda item: item.id):
+        if (ngbh.id not in routes or routes[ngbh.id].cost > curr_cost + ngbh.cost) and ngbh.id != start:
+            routes[ngbh.id] = Route(curr_cost + ngbh.cost, [])
+            print(f"added route to {ngbh.id}: {routes[ngbh.id]}")
+    completed.append(current)
+
 
 src = 2
 dst = 5
-
-def get_routes(g, current, routes, to_visit, src):
-    if current not in routes:
-        curr_cost = 0
-    else: curr_cost = routes[current]
-    print(f"Зашел в узел {current}: routes = {routes}, to_visit = {to_visit}, curr_cost = {curr_cost}")
-    if not to_visit:
-        return
-    if g[current]:
-        nghbrs = sorted(g[current], key=lambda item: item.id)
-        print(f"\tВижу соседей: {nghbrs}")
-        for nghb in nghbrs:
-            if (nghb.id not in routes or routes[nghb.id] > curr_cost + nghb.cost) and nghb.id != src:
-                routes[nghb.id] = curr_cost + nghb.cost
-                print(f"\tДобавил маршрут {nghb.id} со стоимостью {routes[nghb.id]}")
-
-        to_visit.remove(current)
-        for nghb in nghbrs:
-            if nghb.id in to_visit:
-                get_routes(g, nghb.id, routes, to_visit, src)
-    else:
-        print("Соседей нет")
-        to_visit.remove(current)
-        return
-
-
-to_visit = sorted(g.keys())
 routes = {}
-get_routes(g, src, routes, to_visit, src)
+completed = []
+
+process_ngbhrs(g, src, src, routes, completed)
+idle_cycle = False
+while not idle_cycle:
+    idle_cycle = True
+    this_cycle = copy(routes).keys()
+    for vertex in this_cycle:
+        if vertex not in completed:
+            process_ngbhrs(g, src, vertex, routes, completed)
+            idle_cycle = False
 print(routes)
